a2afe994aef5f35ff901c36d65610ae56fa932a4
filyys <93247526+Filipsys@users.noreply.github.com>
2025-09-05 00:24:33 +0200

    Configure Vim settings and key mappings

--- init.lua
+++ init.lua
@@ -1,10 +1,20 @@
+vim.g.mapleader = " "
+
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
+
 vim.o.number = true
 vim.o.ruler = true
+
 vim.o.autoindent = true
 vim.o.smartindent = true
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
+
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.keymap.set("n", "<leader>e", function()
+  vim.cmd("Lexplore!")
+end)

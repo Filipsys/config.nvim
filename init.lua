a2242e7329eb16c0bd2411bde4024a841c4ec5fc
filyys <93247526+Filipsys@users.noreply.github.com>
2025-09-04 10:24:30 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.number = true
+vim.o.ruler = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')

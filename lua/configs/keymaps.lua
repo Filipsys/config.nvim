a7ae7633e324c337800b5a5b66d30fc790b96e7e
Filipsys <filipsysak@gmail.com>
2025-09-25 23:35:51 +0200

    Added a diagnostics keymap & fixed path issues with require()

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,7 +1,8 @@
-vim.keymap.set("n", "<leader>e", function() vim.cmd("vert Lexplore!") end)
+vim.keymap.set("n", "<leader>e", function() vim.cmd("vert Lexplore!") end, { desc = "Open netrw" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>r", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>r", vim.cmd.UndotreeToggle, { desc = "Open undotree" })
 if vim.fn.has("persistent_undo") == 1 then
   local target_path = vim.fn.expand("~/.undodir")
 

c820c6e6bac483ebb5977be6fd3a3c68eb03dadc
Filipsys <filipsysak@gmail.com>
2025-12-18 00:34:01 +0100

    Added print macros for C types

--- lua/configs/macros.lua
+++ lua/configs/macros.lua
@@ -1,3 +1,8 @@
-local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
-vim.keymap.set("v", "<leader>javac",  "yoSystem.out.println(\"" .. esc .. "pa: \" + " .. esc .. "pa);" .. esc)
-vim.keymap.set("v", "<leader>jsc", "yoconsole.log(\"" .. esc .. "pa: \", " .. esc .. "pa);")
+vim.keymap.set("v", "<leader>javac",  "yoSystem.out.println(\"<Esc>pa: \" + <Esc>pa);<Esc>")
+vim.keymap.set("v", "<leader>jsc", "yoconsole.log(\"<Esc>pa: \", <Esc>pa);")
+
+vim.keymap.set("v", "<leader>cdc", "yoprintf(\"%d\\n\", <Esc>pa);<Esc>")
+vim.keymap.set("v", "<leader>cfc", "yoprintf(\"%lf\\n\", <Esc>pa);<Esc>")
+vim.keymap.set("v", "<leader>csc", "yoprintf(\"%s\\n\", <Esc>pa);<Esc>")
+vim.keymap.set("v", "<leader>chc", "yofor (int i = 0; <Esc>pa[i] != '\\0'; i++) {<CR>printf(\"%02X \", <Esc>pa[i]);<CR>}<CR>printf(\"\\n\");<Esc>")
+vim.keymap.set("v", "<leader>ccc", "yoprintf('%c\\n', <Esc>pa);<Esc>")
